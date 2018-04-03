e7ee8a917572a6766ed20238f68b13bf99082a77
Cesar <cga@softtelecom.eu>
2018-04-03 13:00:26 +0200

    Bug fixed, this closes #2

--- lib/refraner_server/model/vote.ex
+++ lib/refraner_server/model/vote.ex
@@ -1,11 +1,26 @@
 defmodule RefranerServer.Model.Vote do
   use Ecto.Schema
 
-  alias __MODULE__
-
   schema "votes" do
     field(:tg_user_id, :integer)
     field(:refran_id, :integer)
     field(:vote, :integer)
+  end
+
+  def changeset(vote, params \\ %{}) do
+    vote
+    |> Ecto.Changeset.cast(params, [:vote])
+  end
+
+  def update_vote(vote, new_vote) do
+    changeset(vote, %{vote: new_vote})
+    |> RefranerServer.Repo.update()
+  end
+
+  def insert_vote(vote) do
+    case RefranerServer.Repo.insert(vote) do
+      {:ok, _} = res_ok -> res_ok
+      _ -> {:error, "Could not insert vote #{inspect(vote)}"}
+    end
   end
 end
